1620c3d73e505723e3523b4c90f6a8c02f9fecf0
Santos Joao (7) <joao.santos.7@volvo.com>
2024-09-13 21:58:33 -0300

    Implemented extension and sample project.

---
+++ src/Aspire.Hosting.MockServer/MockServerResourceBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Lifecycle;
+
+namespace Aspire.Hosting.MockServer;
+
+public static class MockServerResourceBuilderExtensions
+{
+    public static IResourceBuilder<MockServerResource> AddMockServer(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        int? httpPort = null)
+    {
+        // The AddResource method is a core API within .NET Aspire and is
+        // used by resource developers to wrap a custom resource in an
+        // IResourceBuilder<T> instance. Extension methods to customize
+        // the resource (if any exist) target the builder interface.
+        var resource = new MockServerResource(name);
+
+        return builder.AddResource(resource)
+                      .WithImage("mockserver/mockserver")
+                      .WithImageRegistry("docker.io")
+                      .WithHttpEndpoint(
+                          targetPort: 1080,
+                          port: httpPort);
+    }
+
+    public static IResourceBuilder<MockServerResource> WithExpectation(this IResourceBuilder<MockServerResource> builder, string expectation)
+    {
+        builder.ApplicationBuilder.Services.TryAddLifecycleHook<MockServerConfigHook>();
+        builder.Resource.Expectations.Add(expectation);
+        return builder;
+    }
+}
